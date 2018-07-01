aeefdfc3eac583bdc1c8d403cf7edbc1da1d94d1
wangf0228 <13009999100@163.com>
2018-07-01 11:46:08 +0800

    整合钥匙辅助计算器

---
+++ 钥匙辅助计算器/钥匙辅助计算器/Form1.Designer.cs
@@ -0,0 +1,503 @@
+﻿namespace 钥匙辅助计算器
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox10 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox12 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox13 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox11 = new System.Windows.Forms.TextBox();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.tabPage3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(583, 393);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.textBox4);
+			this.tabPage1.Controls.Add(this.textBox5);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.textBox3);
+			this.tabPage1.Controls.Add(this.label6);
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.label5);
+			this.tabPage1.Controls.Add(this.label4);
+			this.tabPage1.Controls.Add(this.label2);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.numericUpDown1);
+			this.tabPage1.Controls.Add(this.textBox2);
+			this.tabPage1.Controls.Add(this.textBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(575, 367);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "滚步";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.textBox6);
+			this.tabPage2.Controls.Add(this.button3);
+			this.tabPage2.Controls.Add(this.button4);
+			this.tabPage2.Controls.Add(this.textBox7);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(575, 367);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "钥匙次数计算器";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(78, 182);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(163, 41);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "计算";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(78, 119);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1454759936,
+            465,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(116, 21);
+			this.numericUpDown1.TabIndex = 5;
+			this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(78, 78);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(163, 21);
+			this.textBox2.TabIndex = 3;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(78, 32);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(163, 21);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "01 02 03 04 05 06 07 08";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(22, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 12);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Hash值";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(22, 81);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 12);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "滚步结果";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(19, 121);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(53, 12);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "滚步次数";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(345, 131);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(166, 21);
+			this.textBox4.TabIndex = 10;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(345, 90);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(166, 21);
+			this.textBox5.TabIndex = 11;
+			this.textBox5.Text = "01 02 03 04 05 06 07 08";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(362, 177);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(129, 50);
+			this.button2.TabIndex = 9;
+			this.button2.Text = "开始";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(345, 32);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(166, 21);
+			this.textBox3.TabIndex = 8;
+			this.textBox3.Text = "01 02 03 04 05 06 07 08";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(286, 35);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(41, 12);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Hash值";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(286, 93);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(53, 12);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "滚步结果";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(286, 134);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(53, 12);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "滚步次数";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(100, 190);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 21);
+			this.textBox6.TabIndex = 6;
+			this.textBox6.Text = "196607";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(178, 127);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "计算寄存器";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(73, 127);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "计算数值";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(100, 58);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 21);
+			this.textBox7.TabIndex = 3;
+			this.textBox7.Text = "7f 1f 2f";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.button5);
+			this.tabPage3.Controls.Add(this.textBox8);
+			this.tabPage3.Controls.Add(this.label7);
+			this.tabPage3.Controls.Add(this.textBox9);
+			this.tabPage3.Controls.Add(this.label8);
+			this.tabPage3.Controls.Add(this.textBox10);
+			this.tabPage3.Controls.Add(this.label9);
+			this.tabPage3.Controls.Add(this.textBox11);
+			this.tabPage3.Controls.Add(this.label10);
+			this.tabPage3.Controls.Add(this.textBox12);
+			this.tabPage3.Controls.Add(this.label11);
+			this.tabPage3.Controls.Add(this.textBox13);
+			this.tabPage3.Controls.Add(this.label12);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(575, 367);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "RF数据计算器";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(140, 222);
+			this.textBox8.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.ReadOnly = true;
+			this.textBox8.Size = new System.Drawing.Size(171, 21);
+			this.textBox8.TabIndex = 8;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(61, 225);
+			this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(65, 12);
+			this.label7.TabIndex = 7;
+			this.label7.Text = "RF发送数据";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(378, 78);
+			this.textBox9.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.ReadOnly = true;
+			this.textBox9.Size = new System.Drawing.Size(171, 21);
+			this.textBox9.TabIndex = 11;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(332, 80);
+			this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(29, 12);
+			this.label8.TabIndex = 5;
+			this.label8.Text = "滚步";
+			// 
+			// textBox10
+			// 
+			this.textBox10.Location = new System.Drawing.Point(140, 146);
+			this.textBox10.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox10.Name = "textBox10";
+			this.textBox10.Size = new System.Drawing.Size(44, 21);
+			this.textBox10.TabIndex = 13;
+			this.textBox10.Text = "2b";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(94, 155);
+			this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(29, 12);
+			this.label9.TabIndex = 6;
+			this.label9.Text = "键值";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(73, 117);
+			this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(53, 12);
+			this.label10.TabIndex = 3;
+			this.label10.Text = "按键次数";
+			// 
+			// textBox12
+			// 
+			this.textBox12.Location = new System.Drawing.Point(140, 78);
+			this.textBox12.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox12.Name = "textBox12";
+			this.textBox12.Size = new System.Drawing.Size(171, 21);
+			this.textBox12.TabIndex = 10;
+			this.textBox12.Text = "4B 85 5A FF 2B A8 CD 3E";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(70, 81);
+			this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(53, 12);
+			this.label11.TabIndex = 4;
+			this.label11.Text = "当前Hash";
+			// 
+			// textBox13
+			// 
+			this.textBox13.Location = new System.Drawing.Point(140, 40);
+			this.textBox13.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox13.Name = "textBox13";
+			this.textBox13.Size = new System.Drawing.Size(171, 21);
+			this.textBox13.TabIndex = 9;
+			this.textBox13.Text = "70 00 c1 59 6e 02 9d d2 ";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(97, 43);
+			this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(29, 12);
+			this.label12.TabIndex = 2;
+			this.label12.Text = "密码";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(378, 183);
+			this.button5.Margin = new System.Windows.Forms.Padding(2);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(119, 60);
+			this.button5.TabIndex = 14;
+			this.button5.Text = "计算";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// textBox11
+			// 
+			this.textBox11.Location = new System.Drawing.Point(140, 114);
+			this.textBox11.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox11.Name = "textBox11";
+			this.textBox11.Size = new System.Drawing.Size(44, 21);
+			this.textBox11.TabIndex = 12;
+			this.textBox11.Text = "7F";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(583, 393);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage3.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textBox12;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox13;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.TextBox textBox11;
+	}
+}
+
---
+++ 钥匙辅助计算器/钥匙辅助计算器/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WFNetLib.StringFunc;
+
+namespace 钥匙辅助计算器
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			byte[] clc = WFNetLib.StringFunc.StringsFunction.strToHexByte(textBox1.Text, " ");
+			WFNetLib.lcyHashCal hc = new WFNetLib.lcyHashCal();
+			byte[] ret = hc.HashCal(clc, (int)numericUpDown1.Value);
+			textBox2.Text = WFNetLib.StringFunc.StringsFunction.byteToHexStr(ret, " ");
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			byte[] clc = WFNetLib.StringFunc.StringsFunction.strToHexByte(textBox3.Text, " ");
+			byte[] ret = WFNetLib.StringFunc.StringsFunction.strToHexByte(textBox5.Text, " ");
+			WFNetLib.lcyHashCal hc = new WFNetLib.lcyHashCal();
+			bool bok = false;
+			for (int i = 0; i < 200000; i++)
+			{
+				clc = hc.HashCal(clc);
+				bok = true;
+				for (int j = 0; j < 8; j++)
+				{
+					if (clc[j] != ret[j])
+					{
+						bok = false;
+						break;
+					}
+				}
+				if (bok)
+				{
+					textBox4.Text = (i + 1).ToString();
+					return;
+				}
+			}
+			textBox4.Text = "没找到";
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			byte[] h = StringsFunction.strToHexByte(textBox7.Text, " ");
+			if (h.Length != 3)
+			{
+				MessageBox.Show("输入数据有误");
+				return;
+			}
+			int lift = h[2] * 32 * 128 + h[1] * 128 + h[0];
+			textBox6.Text = lift.ToString();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			int lift;
+			if (!int.TryParse(textBox6.Text, out lift))
+			{
+				MessageBox.Show("输入数据有误");
+				return;
+			}
+			byte[] h = new byte[3];
+			h[2] = (byte)(lift / (32 * 128));
+			lift = lift - h[2] * 32 * 128;
+			h[1] = (byte)(lift / (128));
+			lift = lift - h[1] * 128;
+			h[0] = (byte)lift;
+			textBox7.Text = StringsFunction.byteToHexStr(h, 0, 3, " ");
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			int i;
+			byte[] PSW = StringsFunction.strToHexByte(textBox13.Text, " ");
+			byte[] curHash = StringsFunction.strToHexByte(textBox12.Text, " ");
+			byte KeyTimes = byte.Parse(textBox11.Text);
+			byte keyValue = byte.Parse(textBox10.Text);
+
+			WFNetLib.lcyHashCal hc = new WFNetLib.lcyHashCal();
+			byte[] ram93 = hc.HashCal(curHash);
+			byte[] KeyData = new byte[8];
+
+			for (i = 0; i < 8; i++)
+				KeyData[i] = (byte)(ram93[i] ^ PSW[i]);
+			//6
+			KeyData[0] ^= ram93[0];
+			KeyData[6] ^= KeyTimes;
+			KeyData[7] ^= keyValue;
+
+			byte[] h1 = hc.HashCal(KeyData);
+
+			for (i = 0; i < 8; i++)
+				KeyData[i] = (byte)(ram93[i] ^ h1[i]);
+
+			textBox9.Text = StringsFunction.byteToHexStr(ram93, " ");
+			textBox8.Text = StringsFunction.byteToHexStr(KeyData, " ");
+		}
+	}
+}
